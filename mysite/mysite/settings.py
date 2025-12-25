458ec748b8c6feb53d1fad5b387f983b441d23d7
dabil <feofeonf@yandex.ru>
2025-12-25 20:23:29 +0300

    Подключил frontend

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "shopapp.apps.ShopappConfig"
+    "shopapp.apps.ShopappConfig",
+    'frontend',
 ]
 
 MIDDLEWARE = [
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("frontend.urls")),
     path("admin/", admin.site.urls),
     path("shop/", include("shopapp.urls"))
 ]

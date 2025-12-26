2c7ba088e3209735d191ecd0597612166396fc59
dabil <feofeonf@yandex.ru>
2025-12-26 18:16:50 +0300

    Привязал модель Avatar к User

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Tag(models.Model):
@@ -7,6 +8,7 @@ def gen_path_to_avatar(instance: 'Avatar', filename: str) -> str:
     return f"avatars/{instance.pk}/{filename}"
 
 class Avatar(models.Model):
-    image = models.ImageField(upload_to=gen_path_to_avatar)
+    image = models.ImageField(upload_to=gen_path_to_avatar, null=True, blank=True)
     alt = models.CharField(max_length=255, blank=True)
 
+    user = models.OneToOneField(User, related_name="avatar", on_delete=models.CASCADE, null=True, blank=True)

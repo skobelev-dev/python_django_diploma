befbca0e3f11e52e73ad0a6367feada557a0d2d4
dabil <feofeonf@yandex.ru>
2025-12-26 18:03:22 +0300

    Добавил модель для аватарок

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 
+from .settings import DEBUG, MEDIA_URL, MEDIA_ROOT
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,3 +25,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("shop/", include("shopapp.urls"))
 ]
+
+if DEBUG:
+    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -1,5 +1,12 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 class Tag(models.Model):
     name = models.CharField(max_length=30)
+
+def gen_path_to_avatar(instance: 'Avatar', filename: str) -> str:
+    return f"avatars/{instance.pk}/{filename}"
+
+class Avatar(models.Model):
+    image = models.ImageField(upload_to=gen_path_to_avatar)
+    alt = models.CharField(max_length=255, blank=True)
+

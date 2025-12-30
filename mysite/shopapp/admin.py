d54e0cb6d4c0e38644dfcfce7265b1abce8ac276
dabil <feofeonf@yandex.ru>
2025-12-30 20:02:06 +0300

    Добавил модели в админ-панель

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Avatar, Payment, Category, Review, ProductImage, Product, Order
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Avatar)
+class AvatarAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = "name",
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "created_at",
+        "user",
+        "deliveryType",
+        "paymentType",
+        "totalCost",
+        "status",
+        "city",
+        "address",
+    )

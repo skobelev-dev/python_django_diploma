721357dd6a3801716058e5e68f931798d7920918
dabil <feofeonf@yandex.ru>
2025-12-30 19:00:26 +0300

    Добавил модель заказа

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -86,3 +86,28 @@ class Product(models.Model):
     freeDelivery = models.BooleanField()
     tags = models.ManyToManyField(Tag, related_name="products")
     reviews = models.ManyToManyField(Review, related_name="products")
+
+
+class Satus(models.TextChoices):
+    ACCEPTED = 'accepted'
+    NOT_ACCEPTED = 'not accepted'
+
+class DeliveryType(models.TextChoices):
+    FREE = 'free', 'Бесплатная доставка'
+    PAID = 'paid', 'Платная доставка'
+
+class PaymentType(models.TextChoices):
+    ONLINE = 'online', 'Онлайн'
+    CASH = 'cash', 'Наличными'
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Как я полагаю: имя, почта и телефон - пользователя совершившего заказ
+    user = models.ForeignKey(User, related_name="orders", on_delete=models.DO_NOTHING)
+    deliveryType = models.CharField(max_length=20, choices=DeliveryType.choices, default=DeliveryType.FREE)
+    paymentType = models.CharField(max_length=20, choices=PaymentType.choices, default=PaymentType.ONLINE)
+    totalCost = models.DecimalField(max_digits=10**9, decimal_places=2)
+    status = models.CharField(max_length=20, choices=Satus.choices, default=Satus.ACCEPTED)
+    city = models.CharField(max_length=30)
+    address = models.CharField(max_length=70)
+    products = models.ManyToManyField(Product, related_name="orders")

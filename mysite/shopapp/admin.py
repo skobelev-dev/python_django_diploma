15fa02b0ccd3cf1b215b1551fc0daa31c4cdafa0
dabil <feofeonf@yandex.ru>
2026-01-09 18:42:47 +0300

    Добавил спецификации в админ-панель

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Tag, Avatar, Payment, Category, Review, ProductImage, Product, Order
+from .models import Specifications, Tag, Avatar, Payment, Category, Review, ProductImage, Product, Order
 
+@admin.register(Specifications)
+class SpecificationAdmin(admin.ModelAdmin):
+    pass
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -25,7 +28,7 @@ class PaymentAdmin(admin.ModelAdmin):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = "name",
+    list_display = ("name",)
 
 
 @admin.register(ProductImage)

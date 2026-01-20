560b5034448f5960de7a2f52c1418442fbc174ca
dabil <feofeonf@yandex.ru>
2026-01-20 19:27:18 +0300

    Написал POST метод для ресурса /profiles

--- mysite/my_auth/serializers.py
+++ mysite/my_auth/serializers.py
@@ -2,41 +2,24 @@ from django.contrib.auth.models import User
 from django.db.models import QuerySet
 from rest_framework import serializers
 
+import mysite.settings
 from my_auth.models import Profile
 
+class AvatarSerializer(serializers.Serializer):
+    src = serializers.FilePathField(path=mysite.settings.MEDIA_ROOT, allow_blank=True, allow_null=True)
+    alt = serializers.CharField(allow_blank=True)
 
-def get_profile(obj):
-    obj_class = obj.__class__
-    if obj_class == QuerySet:
-        return obj[0]
-    return obj
-
+class MyProfileSerializer(serializers.Serializer):
+    fullName = serializers.CharField(max_length=100)
+    email = serializers.EmailField(allow_blank=True)
+    phone = serializers.CharField(max_length=20, required=False)
+    avatar = AvatarSerializer()
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
+        model = Profile
+        fields = 'fullName', 'email', 'phone', 'avatar'
 
-        model = User
-        fields = "fullName", "email", "phone", "avatar"
-
-    fullName = serializers.SerializerMethodField()
-    phone = serializers.SerializerMethodField()
-    avatar = serializers.SerializerMethodField()
-
-    # noinspection PyPep8Naming
-    # noinspection PyMethodMayBeStatic
-    def get_fullName(self, obj: Profile | QuerySet[Profile]):
-        profile = get_profile(obj)
-        return profile.user.username
-
-    # noinspection PyMethodMayBeStatic
-    def get_phone(self, obj: Profile | QuerySet[Profile]):
-        profile = get_profile(obj)
-        return str(profile.phone)
-
-    # noinspection PyMethodMayBeStatic
-    def get_avatar(self, obj: Profile | QuerySet[Profile]):
-        profile = get_profile(obj)
-        avatar = profile.user.avatar or None
-        image = avatar.image or None
-        path = image.path if image else ""
-        return {"src": path, "alt": avatar.alt or ""}
+    fullName = serializers.CharField(source="user.username")
+    email = serializers.EmailField(source="user.email")
+    avatar = AvatarSerializer()
--- mysite/my_auth/views.py
+++ mysite/my_auth/views.py
@@ -1,22 +1,49 @@
 # from django.shortcuts import render
+from django.contrib.auth.models import User
 from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
-
+from rest_framework.viewsets import ViewSet, GenericViewSet
+from django.db.models import F
 from my_auth.models import Profile
-from my_auth.serializers import ProfileSerializer
+from my_auth.serializers import MyProfileSerializer, ProfileSerializer
 
 
 class ProfileViewSet(ViewSet):
-    """
-    Получение списка тегов.
-    """
 
     queryset = Profile.objects.all()
-
+    serializer_class = ProfileSerializer
 
     def list(self, request):
-        profiles = self.queryset
-        count = profiles.count()
-        many = True if count > 1 else False
-        serializer = ProfileSerializer(profiles, many=many)
+
+        response = []
+        for profile in self.queryset:
+            fullName = profile.user.username
+            email = profile.user.email
+            phone = str(profile.phone)
+            image =  profile.user.avatar.image
+            src = "" if not image else image.url or ""
+            avatar = {
+                "src": src,
+                "alt": profile.user.avatar.alt
+            }
+            serialized = MyProfileSerializer(
+                data={
+                    "fullName": fullName,
+                    "email": email,
+                    "phone": phone,
+                    "avatar": avatar
+                }
+            )
+            serialized.is_valid(raise_exception=True)
+            response.append(serialized.data)
+
+        return Response(response)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.data
+        user = User(username=data.get("fullName"), email=data.get("email"))
+        user.save()
+        profile = Profile(phone=data.get("phone"), user=user)
+        profile.save()
         return Response(serializer.data)
